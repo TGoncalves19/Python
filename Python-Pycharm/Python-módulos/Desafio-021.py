271645353fb9aa1d5c3b61b5e842ad0c7ae3c3d8
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-10-21 19:41:43 -0300

    subindo-desafio-021.py

--- Python-Pycharm/Python-módulos/Desafio-021.py
+++ Python-Pycharm/Python-módulos/Desafio-021.py
@@ -1,1 +1,13 @@
 # Desafio 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+'''from pygame import mixer
+mixer.init()
+mixer.music.load('ex021.mp3')
+mixer.music.play()
+input()'''
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+input()

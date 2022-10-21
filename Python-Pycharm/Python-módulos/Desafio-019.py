7c14151b571d0da04992eb119de69b3d65607e84
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-10-21 17:59:10 -0300

    subindo-desafio-019.py

--- Python-Pycharm/Python-módulos/Desafio-019.py
+++ Python-Pycharm/Python-módulos/Desafio-019.py
@@ -2,6 +2,10 @@
 # ajude ele, lendo o nome deles e escrevendo o nome escolhido.
 
 import random
-import string
-nome = (Gabriela, Gustavo, MariaRegina, Manuele)
-print(nome)
+a1 = str(input('Primeiro aluno (a): '))
+a2 = str(input('Segundo Aluno (a): '))
+a3 = str(input('Terceiro Aluno (a): '))
+a4 = str(input('Quarto Aluno (a): '))
+sorteio = [a1, a2, a3, a4]
+escolhido = random.choice(sorteio)
+print('O aluno (a) sorteado foi {}.'.format(escolhido))

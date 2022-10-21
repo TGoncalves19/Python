9f0e90790b031bc8e4443c398f680534e915aaeb
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-10-21 17:15:20 -0300

    subindo-desafio-017.py

--- Python-Pycharm/Python-módulos/Desafio-017.py
+++ Python-Pycharm/Python-módulos/Desafio-017.py
@@ -1,6 +1,15 @@
 # Desafio 017 - Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um
 # triângulo, calcule e mostre o comprimento da hipotenusa.
-co = float(input('Digite o valor do cateto oposto: '))
-ca = float(input('Digite o valor da cateto adjacente '))
-tg = co / ca
-print('O comprimento da hipotenusa é {}'.format(tg))
+
+# Cálculo matemático
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Compriemnto do cateto adjacente: '))
+hi = (co** 2 + ca ** 2) **(1/2)
+print('O comprimento da hipotenusa é {:.2f}'.format(hi))
+
+# Importando com MATH (math.hypot(), calcular a hipotenusa).
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Compriemnto do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('O comprimento da hipotenusa é {:.2f}'.format(hi))

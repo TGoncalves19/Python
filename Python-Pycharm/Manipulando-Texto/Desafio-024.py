1d0788bb63448a974efd2dbe13761a35d61ae943
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-10-23 13:37:30 -0300

    subindo-desafio-024

--- Python-Pycharm/Manipulando-Texto/Desafio-024.py
+++ Python-Pycharm/Manipulando-Texto/Desafio-024.py
@@ -1,1 +1,3 @@
 # Desafio 024 - Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+cidade = str(input('Qual cidade que você mora: ')).strip()
+print(cidade[:5].upper() == 'SANTO')

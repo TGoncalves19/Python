f8d4e551aaf9b8f9b1b62a60edcb874f6521238a
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-10-23 11:29:29 -0300

    subindo-manipulando-texto-09.py

--- Python-Pycharm/Python-módulos/Desafio-021.py
+++ Python-Pycharm/Python-módulos/Desafio-021.py
@@ -5,9 +5,9 @@
 #mixer.music.play()
 #input()
 
-#import pygame
-#pygame.init()
-#pygame.mixer.music.load('ex021.mp3')
-#pygame.mixer.music.play()
-#pygame.event.wait()
-#input()
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+input()
